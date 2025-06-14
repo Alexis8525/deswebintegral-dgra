5a4e8acfd7ce42dc8daade82b529401a11508659
Alexis8525 <alexisdgalindo88@gmail.com>
2025-06-13 19:34:32 -0600

    Agrego contenido de webapp-dgra al repositorio principal

---
+++ webapp-dgra/src/componentes/NavBar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">MiSitioWeb</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarScroll">
+          <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Servicios</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Acerca de</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contacto</a>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Buscar" />
+            <button className="btn btn-outline-light" type="submit">Buscar</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
